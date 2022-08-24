9c3e8938a7a79ca21d50b74bc6bab93404fc4f4b
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-08-24 14:23:51 +0300

    feat: add styles to Main.tsx

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -4,8 +4,15 @@ import s from './Main.module.css'
 const Main = () => {
     return (
         <div className={s.mainBlock} >
-            <div className={s.greeting}></div>
-            <div className={s.photo}></div>
+            <div className={s.container}>
+                <div className={s.greeting}>
+                    <span>Hi There!</span>
+                    <h1>I am Dzmitrii</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
+            </div>
+
         </div>
     );
 };
