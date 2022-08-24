d38db6c09325af9a0cf1b1e0b8623454815bb688
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-08-24 21:07:41 +0300

    style: add styles to Skill.tsx and Skills.tsx

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Skills.module.css'
 import sContainer from './../common files/styles/Container.module.css'
+import Skill from './skill/Skill';
 
 
 const Skills = () => {
@@ -10,7 +11,9 @@ const Skills = () => {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title} >Skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title={'JS'} description={'logcccsflkopccc safccc  ffcccaa ff saaccccccsssgfsdrcs'} />
+                    <Skill title={'TS'} description={'loracscscscaccs'} />
+                    <Skill title={'React'} description={'loa s fg sggggg s gggg sfggcacrcs'} />
                 </div>
             </div>
             
